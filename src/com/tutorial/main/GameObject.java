7094a26ae6ec3529a5b418d66c4aaaa1e376ff32
Yvon MORICE <yvon.morice@epita.fr>
2019-11-04 17:49:42 +0100

    Added GameObject file

---
+++ src/com/tutorial/main/GameObject.java
@@ -0,0 +1,50 @@
+package com.tutorial.main;
+
+import java.awt.*;
+
+public abstract class GameObject {
+
+    protected int x, y, velX, velY;
+    protected ID id;
+
+    public GameObject(int x, int y, ID id) {
+        this.x = x;
+        this.y = y;
+        this.id = id;
+    }
+
+    public abstract void tick();
+    public abstract void render(Graphics g);
+
+    //Getters and setters
+    public int getX() {
+        return x;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+    public int getY() {
+        return y;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+    public int getVelX() {
+        return velX;
+    }
+    public void setVelX(int velX) {
+        this.velX = velX;
+    }
+    public int getVelY() {
+        return velY;
+    }
+    public void setVelY(int velY) {
+        this.velY = velY;
+    }
+    public ID getId() {
+        return id;
+    }
+    public void setId(ID id) {
+        this.id = id;
+    }
+}
